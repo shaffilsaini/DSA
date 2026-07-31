5ada4938715cbc04994deb0f2aab90a4ebeacf14
shaffilsaini <125430486+shaffilsaini@users.noreply.github.com>
2026-08-01 01:25:51 +0530

    Time: 10 ms (48.65%), Space: 25.3 MB (92.36%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        // Step 1: Count frequency of each character
         vector<int> freq(26, 0);
         for (char c : word) {
             freq[c - 'a']++;
         }
 
-        // Step 2: Sort frequencies in DESCENDING order
         sort(freq.begin(), freq.end(), greater<int>());
 
-        // Step 3: Greedily assign characters to keys
         int totalPresses = 0;
         for (int i = 0; i < 26; i++) {
-            if (freq[i] == 0) break;  // No more characters to assign
+            if (freq[i] == 0) break;  
             
-            int pressesNeeded = (i / 8) + 1;  // First 8 get 1 press, next 8 get 2, etc.
+            int pressesNeeded = (i / 8) + 1;  
             totalPresses += freq[i] * pressesNeeded;
         }
 
